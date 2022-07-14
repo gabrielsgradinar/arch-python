a22c549d8a7a953549a7423cf78a2459198198e6
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2022-07-14 13:17:45 -0300

    refact and add deallocate and can_allocate methods

--- model.py
+++ model.py
@@ -1,6 +1,11 @@
 from dataclasses import dataclass
 from datetime import date
-from typing import Optional
+from typing import Optional, Set, NewType
+
+
+Quantity = NewType('Quantity', int)
+Sku = NewType('Sku', str)
+Reference = NewType('Reference', str)
 
 
 @dataclass(frozen=True)
@@ -12,12 +17,31 @@ class OrderLine:
 
 class Batch:
     def __init__(
-        self, ref: str, sku: str, quantity: int, eta: Optional[date]
+        self, ref: Reference, sku: Sku, quantity: Quantity, eta: Optional[date]
     ) -> None:
         self.reference = ref
         self.sku = sku
         self.eta = eta
-        self.available_quantity = quantity
+        self._purchased_quantity = quantity
+        self._allocations: Set[OrderLine] = set()
 
-    def allocate(self, line: OrderLine) -> None:
-        self.available_quantity -= line.quantity
+    def allocate(self, line: OrderLine):
+        if self.can_allocate(line):
+            self._allocations.add(line)
+
+    def deallocate(self, line: OrderLine):
+        if line in self._allocations:
+            self._allocations.remove(line)
+
+    @property
+    def allocated_quantity(self) -> int:
+        return sum(line.quantity for line in self._allocations)
+
+    @property
+    def available_quantity(self) -> int:
+        return self._purchased_quantity - self.allocated_quantity
+
+    def can_allocate(self, line: OrderLine) -> bool:
+        return (
+            self.sku == line.sku and self.available_quantity >= line.quantity
+        )
