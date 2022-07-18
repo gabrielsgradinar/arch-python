e42a285f5c310dc92737b665d21d9342c02bd7a5
Gabriel Gradinar <gabrielgradinar@hotmail.com>
2022-07-18 12:35:26 -0300

    add SqlAlchemyRepository

--- repository.py
+++ repository.py
@@ -11,3 +11,17 @@ class AbstractRepository(ABC):
     @abstractmethod
     def get(self, reference) -> Batch:
         raise NotImplementedError
+
+
+class SqlAlchemyRepository(AbstractRepository):
+    def __init__(self, session):
+        self.session = session
+
+    def add(self, batch):
+        self.session.add(batch)
+
+    def get(self, reference):
+        return self.session.query(Batch).filter_by(reference=reference).one()
+
+    def list(self):
+        return self.session.query(Batch).all()
